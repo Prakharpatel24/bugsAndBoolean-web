03d0aa7d0eeceab199f3dc4e7f39bfdbac2cc380
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-15 20:58:00 +0530

    added_view_profile_api

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,8 +1,37 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet, useNavigate } from "react-router";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/slice/userSlice";
 
 const Body = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { userInfo } = useSelector((store) => store?.user);
+    console.log(userInfo);
+
+    const fetchUser = async () => {
+        try {
+            const res = await axios.get(
+                BASE_URL + "/profile/view",
+                {
+                    withCredentials: true
+                }
+            );
+            dispatch(addUser(res?.data));
+        } catch (error) {
+            if (error?.status === 401) navigate("/login");
+            else console.log("ERROR:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
     return (
         <>
             <Navbar />
