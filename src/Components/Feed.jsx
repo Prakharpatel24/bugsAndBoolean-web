7c9d62c08fc3e9158609f51858e9d67e9dbc6fa9
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-16 13:57:51 +0530

    component_updated

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,7 +1,35 @@
-const Feed = ()=> {
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/slice/feedSlice";
+import UserCardFeed from "./UserCardFeed";
+
+const Feed = () => {
+    const feedData = useSelector((store) => store.feed);
+    console.log(feedData, "feedData");
+    const dispatch = useDispatch();
+
+    const getFeed = async () => {
+        try {
+            if (feedData) return;
+            const res = await axios.get(BASE_URL + "/user/feed", { withCredentials: true });
+            console.log(res?.data, "feedRes");
+            dispatch(addFeed(res?.data));
+        } catch (error) {
+            console.log("ERROR:", error);
+        }
+    }
+    useEffect(() => {
+        getFeed()
+    }, [])
+
     return (
         <>
-        <h1>Feed</h1>
+            {feedData &&
+                <div className="flex justify-center items-center p-30">
+                    <UserCardFeed user={feedData[0]} />
+                </div>}
         </>
     )
 }
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,44 @@
-const Profile = ()=> {
+import axios from "axios";
+import EditProfile from "./EditProfile";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/slice/userSlice";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+const Profile = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store) => store.user.userInfo);
+    // console.log(userData, "UD");
+
+    const fetchUser = async () => {
+        try {
+            if (userData) return;
+            const res = await axios.get(
+                BASE_URL + "/profile/view",
+                {
+                    withCredentials: true
+                }
+            );
+            dispatch(addUser(res?.data));
+        } catch (error) {
+            if (error?.response?.data?.status === 401) navigate("/login");
+            else console.log("ERROR:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+
     return (
-        <>
-        <h1>Profile</h1>
-        </>
+        <div className="flex justify-center p-10">
+            {userData &&
+                <EditProfile user={userData.data} />
+            }
+        </div>
     )
 }
 
