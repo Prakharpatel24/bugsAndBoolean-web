af978bdaf3c4ecefc72f75d862971e1e24fd003e
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-18 20:20:25 +0530

    added_routing_link_for_connection_page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -61,7 +61,11 @@ const Navbar = () => {
                                     <span className="badge">New</span>
                                 </Link>
                             </li>
-                            <li><a>Settings</a></li>
+                            <li>
+                                <Link to="/connections">
+                                    Connections
+                                </Link>
+                            </li>
                             <li onClick={handleLogoutBtnClick}><a>Logout</a></li>
                         </ul>}
                 </div>
