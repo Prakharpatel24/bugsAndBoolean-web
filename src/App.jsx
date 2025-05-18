aa31dfbe99b92bb5a9c52f0254969def2ae24932
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-19 02:54:06 +0530

    added_path_for_requests_page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Feed from './Components/Feed'
 import Profile from './Components/Profile'
 import { ToastContainer } from 'react-toastify'
 import Connections from './Components/Connections'
+import Requests from './Components/Requests'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <Route path='login' element={<Login />} />
               <Route path='profile' element={<Profile />} />
               <Route path='connections' element={<Connections />} />
+              <Route path='requests' element={<Requests />} />
             </Route>
           </Routes>
         </BrowserRouter>
