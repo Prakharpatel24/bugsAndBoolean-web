f068616e0bf0881ef3a9b4d0c38c2c48a29b9971
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-18 20:21:37 +0530

    added_route_for_connections_page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import appStore from './utils/store/appStore'
 import Feed from './Components/Feed'
 import Profile from './Components/Profile'
 import { ToastContainer } from 'react-toastify'
+import Connections from './Components/Connections'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
               <Route path='/' element={<Feed />} />
               <Route path='login' element={<Login />} />
               <Route path='profile' element={<Profile />} />
+              <Route path='connections' element={<Connections />} />
             </Route>
           </Routes>
         </BrowserRouter>
