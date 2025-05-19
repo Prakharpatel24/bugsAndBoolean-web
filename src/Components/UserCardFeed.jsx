c177b2409afeb73023d9b6609bee990a9aad6f77
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-19 06:45:11 +0530

    added_feat_to_send_or_ignore_connection

--- src/Components/UserCardFeed.jsx
+++ src/Components/UserCardFeed.jsx
@@ -1,7 +1,39 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeFromFeed } from "../utils/slice/feedSlice";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
+
 const UserCardFeed = ({ user, showConnectionButtons }) => {
     console.log(user, "user");
-    const { firstName, lastName, age, gender, about, photoURL, skills } = user;
+    const { _id, firstName, lastName, age, gender, about, photoURL, skills } = user;
     console.log(about, "about");
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleConnectionButtonClick = async (status, _id) => {
+        try {
+            const url = `${BASE_URL}/request/send/${status}/${_id}`;
+            const res = await axios.post(
+                url,
+                {},
+                { withCredentials: true }
+            )
+            if (res?.data?.status === 200) {
+                dispatch(removeFromFeed(_id));
+            }
+        } catch (err) {
+            if (err?.response?.data?.status === 401) {
+                navigate("/login");
+            }
+            if (err?.response?.data?.status !== 200) {
+                toast.error(err?.response?.data?.message);
+            }
+            console.log('ERROR:', err);
+        }
+    }
     return (
         <div className="card bg-base-300 w-96 shadow-sm p-5">
             <figure>
@@ -26,8 +58,18 @@ const UserCardFeed = ({ user, showConnectionButtons }) => {
                 <span className="font-mono">About: {about}</span>
                 <p></p>
                 {showConnectionButtons && <div className="card-actions justify-between">
-                    <button className="btn bg-red-600 font-mono">Git Ignore</button>
-                    <button className="btn bg-green-600 font-mono">Let's Git It</button>
+                    <button
+                        className="btn bg-red-600 hover:bg-red-700"
+                        onClick={() => handleConnectionButtonClick("ignored", _id)}
+                    >
+                        Git Ignore
+                    </button>
+                    <button
+                        className="btn bg-green-600 hover:bg-green-700"
+                        onClick={() => handleConnectionButtonClick("interested", _id)}
+                    >
+                        Let's Git It
+                    </button>
                 </div>}
             </div>
         </div>
