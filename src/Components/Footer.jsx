3d2af5d868771318692327e177167db4a56ced2e
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-21 01:04:31 +0530

    content_update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="fixed bottom-0 w-full">
         <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-5">
             <aside>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved by TechMate.com</p>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved.</p>
             </aside>
         </footer>
         </div>
