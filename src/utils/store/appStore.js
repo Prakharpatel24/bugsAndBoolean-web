6429e3a87f6fbf7d39e90977cc5d3613cbb77cd4
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-24 19:50:25 +0530

    added_navbarDropdwon reducer

--- src/utils/store/appStore.js
+++ src/utils/store/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../slice/userSlice";
 import feedReducer from "../slice/feedSlice";
+import navbarDropdownSlice from "../slice/navbarDropdownSlice";
 
 const appStore = configureStore({
     reducer: {
         user: userReducer,
-        feed: feedReducer
+        feed: feedReducer,
+        navbarDropdown: navbarDropdownSlice
     }
 });
 
