89c3ea873d9983df5d663e93700bd7b8ea21ae86
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-25 02:47:18 +0530

    added_custom_hook

---
+++ src/utils/customHooks/useFetchConnections.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { BASE_URL } from "../constants";
+import { useDispatch } from "react-redux";
+import { addConnectionData, setNumberOfConnections } from "../slice/connectionsSlice";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+
+const useFetchConnections = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const getConnections = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/user/connections", { withCredentials: true });
+            dispatch(setNumberOfConnections(res?.data?.data?.length));
+            dispatch(addConnectionData(res?.data?.data));
+        } catch (err) {
+            if (err?.response?.data?.status === 401) navigate("/login");
+            if (err?.response?.data?.status !== 200) {
+                toast.error(err?.response?.data?.message);
+            }
+            console.log('ERROR:', err);
+        }
+    }
+
+    useEffect(() => {
+        getConnections();
+    }, []);
+
+    return { getConnections }
+}
+
+export default useFetchConnections;
---
+++ src/utils/customHooks/useFetchRequests.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { BASE_URL } from "../constants";
+import { addRequestData, setNumberOfRequests } from "../slice/requestsSlice";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+
+const useFetchRequests = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const getRequests = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/user/requests", { withCredentials: true });
+            dispatch(setNumberOfRequests(res?.data?.length));
+            dispatch(addRequestData(res?.data));
+        } catch (err) {
+            if (err?.response?.data?.status === 401) {
+                navigate("/login");
+            }
+            if (err?.response?.data?.status !== 200) {
+                toast.error(err?.response?.data?.message);
+            }
+            console.log('Error:', err);
+        }
+    }
+
+    useEffect(() => {
+        getRequests();
+    }, []);
+
+    return { getRequests }
+}
+
+export default useFetchRequests;
